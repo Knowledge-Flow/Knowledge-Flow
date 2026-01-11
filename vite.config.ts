5c3250d3d3edc0a05e8cee9abd43177bf5c6a88b
0x101008 <shtnb@outlook.com>
2026-01-11 14:55:36 +0800

    action

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 确保在 GitHub Pages 等子路径环境下资源路径正确
   build: {
     outDir: 'dist',
-    sourcemap: true,
+    assetsDir: 'assets',
+    sourcemap: false,
   },
   server: {
     port: 3000
